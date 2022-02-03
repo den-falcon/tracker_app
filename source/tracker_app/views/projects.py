efca34fbb4775a833bccec0995af325d18c0efc4
denis-sokolov <den.s.glad@gmail.com>
2022-02-03 14:57:10 +0300

    Исправлено представление ProjectView

--- source/tracker_app/views/projects.py
+++ source/tracker_app/views/projects.py
@@ -1,9 +1,11 @@
+from django.db.models import Q
 from django.urls import reverse
 from django.views.generic import CreateView, UpdateView, DeleteView
-from django.views.generic.detail import SingleObjectMixin
+from django.views.generic.detail import SingleObjectMixin, DetailView
+from django.views.generic.list import MultipleObjectMixin
 
-from tracker_app.forms import ProjectForm
-from tracker_app.models import Project
+from tracker_app.forms import ProjectForm, SearchForm
+from tracker_app.models import Project, Task
 from tracker_app.views.base import SearchView
 
 
@@ -16,21 +18,33 @@ class ProjectsView(SearchView):
     search_fields = ['name__icontains', 'description__icontains']
 
 
-class ProjectView(SearchView, SingleObjectMixin):
-    template_name = 'projects/project-view.html'
+class ProjectView(DetailView, MultipleObjectMixin):
     model = Project
     context_object_name = "project"
+    template_name = 'projects/project-view.html'
     paginate_by = 2
     paginate_orphans = 0
-    search_fields = ['summary__icontains', 'description__icontains']
-    object = None
+    search_value = None
+    form = None
 
-    def get_context_data(self, *, object_list=None, **kwargs):
-        self.object = self.get_object()
-        tasks = self.object.Tasks.filter(self.get_query())
+    def get_context_data(self, **kwargs):
+        self.form = self.get_form()
+        self.search_value = self.get_search_value()
+        tasks = self.object.Tasks.filter(Q(summary__icontains=self.search_value) |
+                                         Q(description__icontains=self.search_value))
         context = super().get_context_data(object_list=tasks, **kwargs)
-        context['object'] = self.object
+        context['form'] = SearchForm()
+        if self.search_value:
+            context['form'] = SearchForm(initial={"search": self.search_value})
+            context['search'] = self.search_value
         return context
+
+    def get_form(self):
+        return SearchForm(self.request.GET)
+
+    def get_search_value(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data.get("search")
 
 
 class ProjectCreate(CreateView):
