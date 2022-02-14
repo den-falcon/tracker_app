0378cb5aa8e136eb6bd98ade320191ad60bf8f06
denis-sokolov <den.s.glad@gmail.com>
2022-02-14 11:05:24 +0300

    добавлена валидация email и first name, last name

--- source/accounts/forms.py
+++ source/accounts/forms.py
@@ -1,6 +1,18 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 
 class MyUserCreationForm(UserCreationForm):
+    email = forms.CharField(required=True, label="Email", widget=forms.EmailInput)
+
     class Meta(UserCreationForm.Meta):
         fields = ("username", "password1", "password2", "email", "first_name", "last_name")
+
+    def clean(self):
+        cleaned_data = super().clean()
+        first_name = cleaned_data.get("first_name")
+        last_name = cleaned_data.get("last_name")
+        if not first_name and not last_name:
+            raise ValidationError("Хотя бы одно из полей (first_name, last_name) должно быть заполнено")
+        return cleaned_data
