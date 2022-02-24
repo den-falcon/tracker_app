07a89cfbfb8c8de3799bae9699e4f4abee744593
denis-sokolov <den.s.glad@gmail.com>
2022-02-24 14:15:04 +0300

    Созданы вьюшки для редактирования профиля и пароля, созданны шаблоны для редактирования профиля и пароля, добавленны ссылки для редактирования профиля и пароля в другие раннее созданные вьюшки

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accounts.views import RegisterView, UserDetailView, AccountList
+from accounts.views import RegisterView, UserDetailView, AccountList, UserChangeView, UserPasswordChangeView
 
 app_name = 'accounts'
 
@@ -11,4 +11,6 @@ urlpatterns = [
     path('registration/', RegisterView.as_view(), name="registration"),
     path('<int:pk>/', UserDetailView.as_view(), name='profile'),
     path('list/', AccountList.as_view(), name='accounts-list'),
+    path('change/', UserChangeView.as_view(), name='account-change'),
+    path('password-change/', UserPasswordChangeView.as_view(), name='password-change'),
 ]
